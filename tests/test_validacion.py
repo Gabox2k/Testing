e08a1fe3ab849203f2e5a8f1770cc34d509da743
Gabriel Raidan <sforza1383@gmail,com>
2025-11-28 11:27:42 -0300

    se agrego tes de validacion y se modifico el utils

---
+++ tests/test_validacion.py
@@ -0,0 +1,8 @@
+from utils import validar_mensaje
+
+def test_no_mensajes_largos():
+    mensaje = "s" * 200
+    
+    assert validar_mensaje(mensaje) == False
+    
+    
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ def validar_mensaje(mensaje ):
     if mensaje is None:
         return False
     if len(mensaje.strip()) == 0:
+        return 
+    if len(mensaje) > 150:
         return False
     
-    return True   
+    return True    
